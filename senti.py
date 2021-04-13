9317a331ca212e0733d7c3a778f180d276a8a806
Mollylulu <792733883@qq.com>
2021-04-13 13:47:25 +0800

    update

--- senti.py
+++ senti.py
@@ -1,11 +1,17 @@
 import pandas as pd
 import torch
+import random
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
-  
+from transformers import pipeline
+from lime.lime_text import LimeTextExplainer
+
 tokenizer = AutoTokenizer.from_pretrained("nlptown/bert-base-multilingual-uncased-sentiment")
-
 model = AutoModelForSequenceClassification.from_pretrained("nlptown/bert-base-multilingual-uncased-sentiment")
+# classifier = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer, framework='pt')
 
+# explain the model decision
+class_names = ['Very Positive', 'Positive', 'Neutral', 'Negative', 'Very Negative']
+explainer = LimeTextExplainer(class_names=class_names)
 
 reviews = pd.read_csv('./data/total_stars_text.csv')
 
@@ -14,21 +20,23 @@ stars = reviews['stars']
 
 output_file = open('./data/senti.csv', 'w')
 
-for i in range(len(reviews)):
-  t = text[i]
-  s = stars[i]
+# for i in range(len(reviews)):
+#   t = text[i]
+#   s = stars[i]
 
-  try:
-    encoded_input = tokenizer(t, return_tensors='pt')
+idx = random.randrange(0, len(reviews))
+
+
+def predict(text):
+    encoded_input = tokenizer(text, return_tensors='pt')
     output = model(**encoded_input)
-    output['logits']
-  except Exception as e:
-    print(e)
-    continue
+    prob = torch.softmax(output['logits'], dim=1).detach()[0]
+    prob = ','.join(['%.4f' % prob[idx] for idx in range(5)])
+    return prob
 
-  prob = torch.softmax(output['logits'], dim=1).detach()[0]
-  prob = ','.join(['%.4f' % prob[idx] for idx in range(5)])
 
-  out_str = '%d,%d,%s' % (i, s, prob)
-  output_file.write(out_str + '\n')
-
+review_to_pred = reviews.iloc(idx)
+exp = explainer.explain_instance(review_to_pred, predict, num_features=20, num_samples=2000)
+exp.show_in_notebook()
+# out_str = '%d,%d,%s' % (i, s, prob)
+# output_file.write(out_str + '\n')
